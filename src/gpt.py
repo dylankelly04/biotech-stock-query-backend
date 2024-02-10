0e88663a66994ed57d5f2db145166af3616e453c
csun365 <chrisun1030@gmail.com>
2024-02-10 02:01:04 -0500

    functions to run inference on gpt3.5

--- src/gpt.py
+++ src/gpt.py
@@ -1,2 +1,25 @@
+from openai import OpenAI
+client = OpenAI(api_key="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")
+
 def generate(symbol: str, query: str) -> list[str]:
-  return []
+  data = fetch_raw_data(symbol)
+  descriptions = get_data(data, "<description>")
+  titles = get_data(data, "<title>")[:-1]
+  descriptions, titles = preprocess_data(descriptions, titles)
+  input_data = ".\n".join([titles[i] + ": " + descriptions[i] for i in range(len(titles))])
+  prompt = """
+  You are to assume the role of a financial analyst. 
+  Using the given news article titles and descriptions, answer the following question."
+  """ + query
+  output = prompt_model(input_data, prompt)
+  return output
+
+def prompt_model(input_data, prompt):
+  response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role": "system", "content": prompt},
+      {"role": "user", "content": input_data},
+    ]
+  )
+  return response.choices[0].message.content
