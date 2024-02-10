5ffa3949caebc5aa29bcba4fdd6fda58c9e93e9c
KingCrusher275 <mmaiti627@gmail.com>
2024-02-10 01:28:53 -0500

    updated stock.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 from gpt import generate
+from stock import get_data
+import requests
 
 app = FastAPI()
+
 
 @app.get("/")
 def health_check() -> dict[str, list[str]]:
     return {"messages": ["Server healthy."]}
 
+
 @app.get("/{symbol}")
 def read_item(symbol: str) -> dict[str, list[str]]:
     return {"messages": generate(symbol)}
+
+
+@app.get("/data")
+def graph_data():
+    ticker = requests.args.get('ticker')
+    time = requests.args.get('time')
+    return get_data(ticker, time)
---
+++ src/stock.py
@@ -0,0 +1,8 @@
+import requests
+import yfinance as yf
+
+
+def get_data(ticker, time):
+    res = yf.Ticker(ticker)
+    hist = res.history(period=time)
+    return hist.to_json()
