11a7c686d43e8f90313d3b1bf80163502cd7a261
Frank Li <frankla0203@outlook.com>
2024-02-10 13:37:49 -0500

    Similar stocks

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from typing import Union
+from fastapi import FastAPI, Response
 
 from gpt import generate
+from similar_stocks import get_recommended_symbols
 from stock import get_data
 
 app = FastAPI()
@@ -12,6 +14,15 @@ def health_check() -> dict[str, list[str]]:
 @app.get("/api/{symbol}")
 def generate_response(symbol: str, query: str) -> dict[str, list[str]]:
     return {"messages": generate(symbol, query)}
+
+@app.get("/api/similar/{symbol}")
+def get_similar(symbol: str, res: Response) -> Union[dict[str, list[str]], str]:
+    try:
+        similars = get_recommended_symbols(symbol)
+        return {"similar": similars}
+    except:
+        res.status_code = 500
+        return "Failed to fetch similar stocks (500)"
 
 @app.get("/data")
 def graph_data(ticker: str, time: str):
