1d350f3601c481682a6c999adc26473bdd007cfc
csun365 <chrisun1030@gmail.com>
2024-02-10 13:51:22 -0500

    include inputs from both yfinance and seeking alpha

--- src/gpt.py
+++ src/gpt.py
@@ -7,7 +7,7 @@ def generate(symbol: str, query: str) -> list[str]:
   titles = get_data(data, "<title>")[:-1]
   descriptions, titles = preprocess_data(descriptions, titles)
   input_data = ".\n".join([titles[i] + ": " + descriptions[i] for i in range(len(titles))])
-  input_data += ".\n".join(get_titles)
+  input_data += ".\n".join(get_titles(symbol))
   prompt = """
   You are to assume the role of a financial analyst. 
   Using the given news article titles and descriptions, answer the following question. "
