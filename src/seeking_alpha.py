19e3545977a2c05423f8922470089b5ddb56ea83
csun365 <chrisun1030@gmail.com>
2024-02-10 16:36:12 -0500

    added dates for seeking alpha

--- src/seeking_alpha.py
+++ src/seeking_alpha.py
@@ -15,10 +15,11 @@ def fetch_rss(symbol: str) -> str:
 def parse_rss(rss: str) -> list[str]:
   soup = bs.BeautifulSoup(rss, "xml")
   items = soup.find_all("item")
-
-  return [item.title.text for item in items]
+  titles = [item.title.text for item in items]
+  dates = [item.pubDate.text[item.pubDate.text.find(", ")+2:item.pubDate.text.find(", ")+13] for item in items]
+  return [titles[i] + " " + dates[i] for i in range(len(titles))]
 
 def get_titles(symbol: str) -> list[str]:
-  print(f"Fetching titles for {symbol}")
+  print(f"Fetching titles and dates for {symbol}")
   rss = fetch_rss(symbol)
   return parse_rss(rss)
