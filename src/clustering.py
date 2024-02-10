eb2678b36b23cf011753669c8d2fb9329d502b0d
Frank Li <frankla0203@outlook.com>
2024-02-10 18:53:30 -0500

    Use sklearn vectorizer instead of hf

--- src/clustering.py
+++ src/clustering.py
@@ -1,16 +1,13 @@
-from sentence_transformers import SentenceTransformer, util
 from sklearn import cluster
+from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
-
 
 def cluster_sentences(sentences: list[str], n_clusters=10):
   print("Clustering sentences...")
   print("Length before:", len(sentences))
-  model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-  encodings = [model.encode(s, convert_to_numpy=True) for s in sentences]
-
-  points = np.array(encodings)
-  clusters = cluster.KMeans(n_clusters=n_clusters).fit(points)
+  vectorizer = TfidfVectorizer()
+  encodings = vectorizer.fit_transform(sentences)
+  clusters = cluster.KMeans(n_clusters=n_clusters).fit(encodings)
   labels = clusters.labels_
 
   indices = np.unique([np.where(labels == i)[0][0] for i in range(n_clusters)])
